4c4380691b55b8cd250f9b64877fea66c78b8e23
vimleshk37 <vimlesh37@gmail.com>
2024-11-04 11:14:58 +0530

    merger local and seleniumgrid base file in one and add new parameter environment in testNG. default environment is local

--- OrangeHRM/src/test/java/base/BaseTest.java
+++ OrangeHRM/src/test/java/base/BaseTest.java
@@ -1,11 +1,15 @@
 package base;
 
 import java.io.IOException;
-
+import java.net.URI;
+import java.net.URL;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
@@ -14,23 +18,37 @@ import org.testng.annotations.Parameters;
 public class BaseTest {
 	public static WebDriver driver;
 
-	@Parameters("browser")
+	@Parameters({ "browser", "environment" })
 	@BeforeTest(alwaysRun = true)
-	public void setup(@Optional("chrome") String browser) throws IOException {
-		if (browser.equals("chrome")) {
-			driver = new ChromeDriver();
-			System.out.println(browser + " Browser is launched");
+	public void setup(@Optional("chrome") String browser, @Optional("local") String environment) throws IOException {
+
+		if (environment.equals("local")) {
+
+			if (browser.equals("chrome")) {
+				driver = new ChromeDriver();
+				System.out.println(browser + " Browser is launched");
+			}
+			if (browser.equals("MicrosoftEdge")) {
+				driver = new EdgeDriver();
+				System.out.println(browser + " Browser is launched");
+			}
+			if (browser.equals("firefox")) {
+				driver = new FirefoxDriver();
+				System.out.println(browser + " Browser is launched");
+			}
+			driver.manage().window().maximize();
+			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		}
-		if (browser.equals("MicrosoftEdge")) {
-			driver = new EdgeDriver();
-			System.out.println(browser + " Browser is launched");
+
+		else if (environment.equals("seleniumgrid")) {
+			URL url = URI.create("http://127.0.0.1:4444").toURL();
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setBrowserName(browser);
+			capabilities.setPlatform(Platform.WIN10);
+			driver = new RemoteWebDriver(url, capabilities);
+			driver.manage().window().maximize();
+			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		}
-		if (browser.equals("firefox")) {
-			driver = new FirefoxDriver();
-			System.out.println(browser + " Browser is launched");
-		}
-		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 	}
 
 	@AfterTest(alwaysRun = true)
